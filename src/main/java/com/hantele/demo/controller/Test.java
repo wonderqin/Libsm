8625c0cc5c24171268dcf9bc6b5cfc75b7ddd088
hanteleSQS <asun888@qq.com>
2017-11-16 16:00:10 +0800

    test sqs3

--- src/main/java/com/hantele/demo/controller/Test.java
+++ src/main/java/com/hantele/demo/controller/Test.java
@@ -1,5 +1,7 @@
 package com.hantele.demo.controller;
 
 public class Test {
-    private static final int i = 5;
+    public static void main(String[] args) {
+        System.out.println("aaa");
+    }
 }
